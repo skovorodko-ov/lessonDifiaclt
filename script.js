e33f4d34a8895aa51b8adb5c67b269219dd5eb24
Oleg Skovorodko <skovorodko29081989@gmail.com>
2021-08-03 18:32:42 +0300

    added lesson04

--- script.js
+++ script.js
@@ -1,51 +1,22 @@
-let lang;
+'use strict';
 
-lang = prompt('Ввидите язык: ru или en');
+// переменные на выбор, из promt всегда приходит строка
 
-// через if
-if (lang === 'ru') {
-    console.log('понедельник, вторник, среда, четверг, пятница, суббота, воскресение');
-} else if (lang === 'en') {
-    console.log('monday, tuesday, wednesday, thursday, friday, saturday, sunday');
-} else {
-    console.log('введено не верное значение');
+// const str = 'hellow';
+// const str = '   hellow    ';
+// const str = 12121;
+const str = '  Привет меня зовут Олег! Мне 31 год. Я родом из Минска.';
+
+function foo(a) {
+    if (typeof(a) !== 'string') {                   // проверка типа
+        return console.log('вы ввели не строку!');  // оповещение пользователя, можно заменить на alert
+    }
+    a = a.trim();                                   // уберает пробелы в начале и конце у переменной
+    if (a.length > 30) {                            // проверяем длинну строки
+        return console.log(a.slice(0, 30) + '...'); // обрезаем до 30 знаков и добовляем "..."
+    } else {
+        return console.log(a);                      // если условия не выполнились возвращаем строку
+    } 
 }
 
-// через switch-case 
-switch (lang) {
-    case 'ru':
-        console.log('понедельник, вторник, среда, четверг, пятница, суббота, воскресение');
-        break;
-    case 'en':
-        console.log('monday, tuesday, wednesday, thursday, friday, saturday, sunday');
-        break;
-    default:
-        console.log('введено не верное значение');
-}
-
-// через многомерный массив
-// первый вариант если использовать объект и ключи
-let array = {};
-
-array.ru = ['понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота', 'воскресение'];
-array.en = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'];
-
-console.log(array[lang]);
-
-// второй вариант если использовать тернарный оператор, что не запрещено условиями задачи, и именно массив
-let array02 = [];
-
-array02[0] = ['понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота', 'воскресение'];
-array02[1] = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'];
-
-let resault = lang === 'ru' ? 0 : lang === 'en' ? 1 : console.log('не верно введено значение');
-console.log(array02[resault]);
-
-// третий вариант если использовать именно массив и без логическихх операторов
-let resault02 = +lang.includes('en'); // вот только не придумал как сообщить что введено не то что надо
-console.log(array02[resault02]);
-
-// вторая часть дополнительного задания
-let namePerson = prompt('ввидите имя');
-
-let resaul03 = namePerson === 'Артем' ? console.log('директор') : (namePerson === 'Максим' ? console.log('преподаватель') : console.log('студент'))
+foo(str);                                           // вызов функции
